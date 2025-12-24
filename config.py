ad8113ba1c46fd166274d7934f0f69e2dcc2fa1d
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-12-24 14:31:28 +0200

    Add: manual speed configuration
    Added manual control speed setting.

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# --- config.py ---
+
 # Налаштування мережі
 DRONE_IP = '192.168.10.1'
 UDP_VIDEO_ADDRESS = 'udp://192.168.10.1:11111'
@@ -11,6 +13,9 @@ PID_COEFFICIENTS = [0.4, 0.4, 0]
 
 # Діапазон площі обличчя для дистанції [min, max]
 FACE_AREA_RANGE = [6200, 6800]
+
+# Швидкість ручного керування (0-100)
+MANUAL_SPEED = 50  # <-- НОВЕ
 
 # Шляхи до файлів
 CASCADE_PATH = "haarcascade_frontalface_default.xml"
