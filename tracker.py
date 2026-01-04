c026f7ae4c7c34f37b21b209fdad6e76fc2f64c8
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2026-01-04 16:24:44 +0200

    Add: Height control for face tracking
    Added height control for face tracking with new variable 'ud'. Updated PID calculations to include height adjustments.

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import cvzone
 from cvzone.FaceMeshModule import FaceMeshDetector
 import cv2
+import numpy as np # Не забудь імпортувати numpy
 import config
 
 class FaceTracker:
@@ -43,30 +44,49 @@ class FaceTracker:
         return img, info
 
     def calculate_pid(self, info):
-        # Ця частина не змінюється
         area = info[1]
         x, y = info[0]
         fb = 0 
-        yaw = 0 
+        yaw = 0
+        ud = 0 # <-- НОВА ЗМІННА ВИСОТИ
         
-        error = x - self.w // 2
-        yaw = self.pid[0] * error + self.pid[1] * (error - self.pError)
+        # --- 1. ПОВОРОТ (Yaw) ---
+        error_yaw = x - self.w // 2
+        yaw = self.pid[0] * error_yaw + self.pid[1] * (error_yaw - self.pError)
         
         # Обмежуємо (clamping)
         if yaw > 100: yaw = 100
         elif yaw < -100: yaw = -100
         else: yaw = int(yaw)
         
-        if -15 < error < 15: 
+        if -15 < error_yaw < 15: 
             yaw = 0
-            error = 0
+            error_yaw = 0 # Скидаємо помилку в мертвій зоні
             
-        self.pError = error
+        self.pError = error_yaw
 
+        # --- 2. ДИСТАНЦІЯ (Forward/Back) ---
         if area != 0: 
             if area < self.fbRange[0]: fb = 25
             elif area > self.fbRange[1]: fb = -25
+
+        # --- 3. ВИСОТА (Up/Down) ---
+        # Центр екрану по висоті
+        error_ud = (self.h // 2) - y
         
-        if x == 0: yaw = 0; fb = 0
+        # Простий пропорційний регулятор (Kp = 0.5)
+        ud = int(error_ud * 0.5)
+        
+        # Обмеження швидкості
+        if ud > 50: ud = 50
+        elif ud < -50: ud = -50
+        
+        # Мертва зона для висоти
+        if -15 < error_ud < 15: ud = 0
+        
+        if x == 0:
+            yaw = 0
+            fb = 0
+            ud = 0
             
-        return yaw, fb
+        return yaw, fb, ud # <-- ПОВЕРТАЄМО 3 ЗНАЧЕННЯ
